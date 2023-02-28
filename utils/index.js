a1a87b734c1c1b6f98e0e0b43fa5940907a5861b
MasonLagrange <mason.lagrange@gmail.com>
2023-02-27 19:16:39 -0500

    added questions

---
+++ utils/index.js
@@ -0,0 +1,18 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+
+// TODO: Create an array of questions for user input
+const questions = ['What is the title of your project?', 
+"Enter a description: ", 'Enter installation instructions: ', 
+'Enter usage information: ', 'Add contribution guidelines: ',
+'Enter test instructions: ', 'Select your license type: ', 
+'Enter your GitHub username: ', 'Enter your e-mail address: '];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
